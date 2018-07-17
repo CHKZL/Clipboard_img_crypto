8a3c13ced10800eea19d2201a5e6dea155b8bbbd
CHKZL <kuanhzilin@gmail.com>
2018-07-17 13:41:06 +0800

    自动加解密剪切板图片

---
+++ 自动加解密剪切板图片/自动加解密剪切板图片/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace 自动加解密剪切板图片
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.T1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "加载的时候为了全局监听快捷键会加载一个键盘钩子";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ctrl+C复制QQ窗口里的图或者任何图片文件";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "按Alt+1加密图片";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(14, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 71);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "加密说明";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "然后Ctrl+V粘贴到任意地方即可";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(14, 143);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(282, 71);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "解密说明";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "然后Ctrl+V粘贴到任意地方即可";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Ctrl+C复制本程序加密的表情";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 12);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "按Alt+2解密图片";
+            // 
+            // T1
+            // 
+            this.T1.Location = new System.Drawing.Point(107, 223);
+            this.T1.Name = "T1";
+            this.T1.Size = new System.Drawing.Size(100, 21);
+            this.T1.TabIndex = 5;
+            this.T1.Text = "114514";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 12);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "加解密随机种子";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 27);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(233, 12);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "太大的图片可能要算一两秒，不要重复操作";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 44);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(257, 12);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "((我这1000*1000的表情大约需要1秒出头的样子";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 250);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.T1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "自动加解密剪切板图片";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox T1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ 自动加解密剪切板图片/自动加解密剪切板图片/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 自动加解密剪切板图片
+{
+    public partial class Form1 : Form
+    {
+        KeyboardHook k_hook;
+        static int key = 114514;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                k_hook = new KeyboardHook();
+                k_hook.KeyDownEvent += new System.Windows.Forms.KeyEventHandler(hook_KeyDown);//钩住键按下 
+                k_hook.KeyPressEvent += K_hook_KeyPressEvent;
+                k_hook.Start();//安装键盘钩子
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("键盘钩子加载失败，请在安全软件里同意一下((\n");
+            }
+           
+        }
+        private void K_hook_KeyPressEvent(object sender, KeyPressEventArgs e)
+        {
+            //tb1.Text += e.KeyChar;
+            int i = (int)e.KeyChar;
+            //  System.Windows.Forms.MessageBox.Show(i.ToString());
+        }
+        private void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)ModifierKeys == (int)Keys.Alt && (e.KeyValue == 49 || e.KeyValue == 50))
+            {
+                try
+                {
+                    int.Parse(T1.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("随机数种子必须为Int32数值");
+                    return;
+                }
+                try
+                {
+                    if (Clipboard.ContainsImage())//判断剪切板有没有图片
+                    {
+                        key = int.Parse(T1.Text);
+                        string[] files = new string[Clipboard.GetFileDropList().Count];
+                        Clipboard.GetFileDropList().CopyTo(files, 0);
+                        foreach (string file in files)
+                        { }
+                        // Image img = Bitmap.FromFile(files[files.Length-1]);
+                        Bitmap BB = new Bitmap(files[files.Length - 1]);
+                        if (BB != null)
+                        {
+                            int Width = BB.Width;
+                            int Height = BB.Height;
+
+                            if (e.KeyValue == 49)
+                            {
+
+                                Stopwatch watch = new Stopwatch();
+                                watch.Start();
+                                int[] list = GetRandomIntValues(Width * Height);
+                                watch.Stop();
+                                Console.WriteLine(watch.Elapsed.TotalSeconds);
+
+                                Bitmap newb = new Bitmap(Width, Height);
+                                for (int i = 0; i < list.Length; i++)
+                                {
+                                    newb.SetPixel((i % Width), (i / Width), BB.GetPixel((list[i] % Width), (list[i] / Width)));
+
+                                }
+                                Image img = newb;
+                                Clipboard.SetImage(img);
+                                // MessageBox.Show("49");
+                            }
+                            else
+                            {
+                                int[] list = GetRandomIntValues(Width * Height);
+                                Bitmap newb = new Bitmap(Width, Height);
+                                for (int i = 0; i < list.Length; i++)
+                                {
+                                    newb.SetPixel((list[i] % Width), (list[i] / Width), BB.GetPixel((i % Width), (i / Width)));
+
+                                }
+                                Image img = newb;
+                                Clipboard.SetImage(img);
+
+                                //    MessageBox.Show("50");
+                            }
+                            Random A = new Random();
+                            // Clipboard.SetImage(img);
+                        }
+                        else
+                        {
+                            MessageBox.Show("null");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("剪切板里没有图片");
+                    }
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("emmmm出现了未知的错误，联系一下我(作者)我来继续魔改魔改((");
+                }
+               
+            }
+        }
+
+        private static string GetBase64(string str)
+        {
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            return Convert.ToBase64String(bytes);
+        }
+
+
+        private static string BitmapGetBase64(string file)
+        {
+            System.Drawing.Bitmap bmp1 = new System.Drawing.Bitmap(file);
+            using (MemoryStream ms1 = new MemoryStream())
+            {
+                bmp1.Save(ms1, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] arr1 = new byte[ms1.Length];
+                ms1.Position = 0;
+                ms1.Read(arr1, 0, (int)ms1.Length);
+                ms1.Close();
+                return Convert.ToBase64String(arr1);
+            }
+        }
+
+        private static Bitmap usBase64(string file)
+        {
+            byte[] arr = Convert.FromBase64String(file);
+            using (MemoryStream ms = new MemoryStream(arr))
+            {
+                Bitmap bmp = new Bitmap(ms);
+                return bmp;
+            }
+        }
+        #region DES 加密解密
+
+        /// <summary>
+        ///  DES 加密
+        /// </summary>
+        /// <param name="source">原文</param>
+        /// <param name="keyVal">秘钥</param>
+        /// <param name="ivVal">向量</param>
+        /// <returns></returns>
+        public static string Des(string source, string keyVal, string ivVal)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(source);
+                var des = new DESCryptoServiceProvider { Key = Encoding.ASCII.GetBytes(keyVal.Length > 8 ? keyVal.Substring(0, 8) : keyVal), IV = Encoding.ASCII.GetBytes(ivVal.Length > 8 ? ivVal.Substring(0, 8) : ivVal) };
+                var desencrypt = des.CreateEncryptor();
+                byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+                return BitConverter.ToString(result);
+            }
+            catch { return "转换出错！"; }
+        }
+
+        /// <summary>
+        /// DES 解密
+        /// </summary>
+        /// <param name="source">原文</param>
+        /// <param name="keyVal">秘钥</param>
+        /// <param name="ivVal">向量</param>
+        public static string UnDes(string source, string keyVal, string ivVal)
+        {
+            try
+            {
+                string[] sInput = source.Split("-".ToCharArray());
+                byte[] data = new byte[sInput.Length];
+                for (int i = 0; i < sInput.Length; i++)
+                {
+                    data[i] = byte.Parse(sInput[i], NumberStyles.HexNumber);
+                }
+                var des = new DESCryptoServiceProvider { Key = Encoding.ASCII.GetBytes(keyVal.Length > 8 ? keyVal.Substring(0, 8) : keyVal), IV = Encoding.ASCII.GetBytes(ivVal.Length > 8 ? ivVal.Substring(0, 8) : ivVal) };
+                var desencrypt = des.CreateDecryptor();
+                byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+                return Encoding.UTF8.GetString(result);
+            }
+            catch { return "解密出错！"; }
+        }
+
+        #endregion
+
+        private int[] GetRandomIntValues(int expectedCouont)
+        {
+            Dictionary<int, int> container = new Dictionary<int, int>(expectedCouont);
+            Random r = new Random(key);
+            while (container.Count < expectedCouont)
+            {
+                int value = r.Next(0, expectedCouont);
+                if (!container.ContainsKey(value))
+                {
+                    container.Add(value, value);
+                }
+            }
+            int[] result = new int[expectedCouont];
+            container.Values.CopyTo(result, 0);
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        /// <summary>
+        /// 执行JS
+        /// </summary>
+        /// <param name="sCode">代码的字符串</param>
+        /// <param name="method">方法名</param>
+        /// <param name="sExpression">参数体</param>
+        /// <param name="language">脚本语言（JScript/VBScript）</param>
+        /// <returns>返回值</returns>
+        public static string ExecuteScript(string sCode, string method, string sExpression, string language)
+        {
+            MSScriptControl.ScriptControl scriptControl = new MSScriptControl.ScriptControl();
+            scriptControl.UseSafeSubset = false;
+            scriptControl.Language = language;
+            scriptControl.AllowUI = true;
+            scriptControl.AddCode(sCode);
+
+            object[] param = sExpression.Split(',');
+
+            object[] param1 = new object[param.Length];
+            for (int i = 0; i < param.Length; i++)
+            {
+                param1[i] = param[i];
+            }
+            try
+            {
+                string str1 = scriptControl.Run(method, param1).ToString();
+                return str1;
+            }
+            catch (COMException comex)
+            {
+                string str = comex.Message;
+            }
+            catch (Exception ex)
+            {
+                string str = ex.Message;
+            }
+            return null;
+        }
+        Form frmOpener;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.Hide(); //这里只是隐藏主窗口，后台在默默加载数据。
+          
+        }
+        public void ShowLogin(Form frmMain)
+        {
+
+            frmOpener = frmMain;
+            this.Show();
+        }
+
+        private void T1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
